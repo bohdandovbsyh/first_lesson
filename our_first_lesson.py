0901225ceb501fa705e7a07322a75973c3aa7ce0
bohdan.dovbysh <bohdan.dovbysh@cyberproof.com>
2023-01-13 13:42:55 +0200

    first commit

---
+++ our_first_lesson.py
@@ -0,0 +1,115 @@
+# Mutable
+# Ctrl + Alt + L
+# my_list = [1, 2, 3, 4, 5]
+# my_list[2] = 55
+# print(my_list)
+# print(my_list)
+# my_list.append(8)
+# print(my_list)
+# my_dict = {'person_1': 'John', 'person_2': 'Marta'}
+# my_set = {1, 2, 3, 4, 5}
+# print(my_set)
+
+# Immutable
+# my_integer = 10
+# my_float = 5.5254
+# my_boolean = True
+# my_tuple = (1, 2, 3)
+# # my_tuple[1] = 55
+# my_str = 'Hello World'
+# frozen_set = frozenset((4, 5, 6, 8, 9, 9))
+# print(frozen_set)
+# my_str[7] = 'YOU'
+
+# my_list = [1, 2, 3, 4, 5]
+# print(id(my_list))
+# my_list.append(9)
+# print(id(my_list))
+# a = 5
+# print(id(a))
+# a = 456465465456
+# print(id(a))
+
+# my_second_tuple = (1, 2, [4, 5, 6])
+# print(my_second_tuple)
+# my_second_tuple[2].append(756)
+# print(my_second_tuple)
+
+# +
+# a = 5
+# b = 2
+
+# result = a + b
+# print(result)
+# # -
+# result = b - a
+# print(result)
+
+# * /
+# result_1 = a * b
+# print(result_1)
+# result_2 = b / a
+# print(result_2)
+
+# %
+# result = a % b
+# print(result)
+
+# //
+# result = a // b
+# print(result)
+
+# a = '1'
+# b = '2'
+#
+# result = a + b
+# print(result)
+# print(type(result))
+
+# a = 1
+# print(type(a))
+# a = str(a)
+# print(type(a))
+# a = int(a)
+# print(type(a))
+
+# my_friends = ['Peter', 'Homer', 'Liza', 'Marge']
+# my_friends = str(my_friends)
+# print(my_friends)
+# my_friends = list(my_friends)
+# print(my_friends)
+
+# my_list_len = len(my_friends)
+# print(my_list_len)
+
+# For loop
+# for index in range(0, len(my_friends)):
+#     print(my_friends[index])
+
+# my_friends = ['Peter', 'Homer', 'Liza', 'Marge']
+# # For each
+# print('BEFORE')
+# for friend in my_friends:
+#     print(friend)
+#
+# print('FINISH')
+
+# While
+
+# counter = 0
+#
+# while True:
+#     print(1)
+#     counter += 1
+#     if counter == 5:
+#         break
+# my_dict = {'person_1': 'John', 'person_2': 'Marta'}
+# for key in my_dict:
+#     print(key)
+#     print(my_dict[key])
+
+# for value in my_dict.items():
+#     print(value)
+#
+# a = [1, 2, 3]
+# c, b, d = [1, 2, 3]
